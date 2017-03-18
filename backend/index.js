aab598bd8c0f3ff8fcc82a9d2b683f509969e870
Josep de Cid <josep.de.cid@gmail.com>
2017-03-18 11:23:37 +0100

    Add microcities

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
+'use strict';
+
 const express = require('express');
-const app = express();
+const fs = require('fs');
 
 const PORT = 3000;
+const app = express();
 
 /*---- Routes ----*/
 
 app.get('/', function (req, res) {
     res.json({message: 'Hello world!'});
+});
+
+app.get('/microCities', function (req, res) {
+    fs.readFile('./resources/micro-cities.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        res.json(JSON.parse(data.toString()));
+    });
 });
 
 /*---- Server ----*/

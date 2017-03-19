8419cfb37cb7a055f4347f2cd38a3845ecc5b021
Francesc de Puig Guixe <francescdepuigguixe@MacBook-Pro-de-Francesc.local>
2017-03-19 02:29:03 +0100

    Add call promotions

--- backend/routes.js
+++ backend/routes.js
@@ -17,27 +17,35 @@ module.exports = function (app) {
             }
         });
     });
-    /*
+    
      app.get('/bigiot/access/promotions', function (req, res) {
-     fs.readFile('./resources/micro-cities.json', 'utf8', function (err, datacities) {
-     const status = treatError(err);
-     if (status === 200) {
-     fs.readFile('./resources/promotions.json', 'utf8', function (err, dataPromotions) {
-     const status = treatError(err);
-     if (status === 200) {
-     const promotions=JSON.parse(dataPromotions.toString());
-     let returnedPromotions[];
-     promotions.forEach();
-     res.json(JSON.parse(dataPromotions.toString()));
-     } else {
-     res.sendStatus(status);
-     }
+         fs.readFile('./resources/micro-cities.json', 'utf8', function (err, datacities) {
+             const status = treatError(err);
+             if (status === 200) {
+                 const microCities = JSON.parse(datacities.toString());
+                 fs.readFile('./resources/promotions.json', 'utf8', function (err, dataPromotions) {
+                     const status = treatError(err);
+                     if (status === 200) {
+                        const promotions=JSON.parse(dataPromotions.toString());
+                        let returnedPromotions=[];
+                        promotions.forEach(function(promotion) {
+                            const microcityID=promotion.microcity-1;
+                            returnedPromotions.push({
+                                "microcity":microCities[microcityID],
+                                "discount":promotion.discount,
+                                "service":promotion.service
+                            });
+                        });
+                        res.json(returnedPromotions);
+                     } else {
+                        res.sendStatus(status);
+                     }
+                 });
+             } else {
+                 res.sendStatus(status);
+             }
+         });
      });
-     } else {
-     res.sendStatus(status);
-     }
-     });
-     });*/
 
     app.get('/bigiot/access/microcities/:id/services', function (req, res) {
         fs.readFile('./resources/micro-cities.json', 'utf8', function (err, data) {
@@ -109,7 +117,7 @@ module.exports = function (app) {
 
     function treatError(err) {
         if (err) {
-            console.log(err);
+            console.log(err.errorType);
             if (err.errorType === 'param_error') return 400;
             else if (err.errorType === 'invalid_auth') return 403;
             else return 500;

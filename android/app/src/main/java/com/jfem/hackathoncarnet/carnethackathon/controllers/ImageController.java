33898ee746c8f67e4f85c0fbdbb684c2ecfa86a5
Marc Vila <marcvilagomez@gmail.com>
2017-03-19 14:01:47 +0100

    ImageController getPhoto working

--- android/app/src/main/java/com/jfem/hackathoncarnet/carnethackathon/controllers/ImageController.java
+++ android/app/src/main/java/com/jfem/hackathoncarnet/carnethackathon/controllers/ImageController.java
@@ -2,22 +2,17 @@ package com.jfem.hackathoncarnet.carnethackathon.controllers;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.util.Base64;
 import android.util.Log;
 import android.widget.ImageView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class ImageController {
 
@@ -48,33 +43,15 @@ public class ImageController {
 
     private static void getPhoto(final Context context, JSONObject res, final ImageView mHeader) throws JSONException {
         final String url = res.getString("prefix") + "500x200" + res.getString("suffix");
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.e("ASD", url);
-                        byte[] decodedString = Base64.decode(response, Base64.DEFAULT);
-                        Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-                        mHeader.setImageBitmap(bitmap);
-                    }
-                }, new Response.ErrorListener() {
 
+        ImageRequest imageRequest = new ImageRequest(url, new Response.Listener<Bitmap>() {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "That didn't work!");
+            public void onResponse(Bitmap response) {
+                mHeader.setImageBitmap(response);
             }
+        }, 0, 0, null, null);
 
-        }) {
-            @Override
-            public Map<String, String> getHeaders() {
-                HashMap<String, String> headers = new HashMap<>();
-                headers.put("Content-Type", "octet/stream");
-                headers.put("Content-Transfer-Encoding", "base64");
-                return headers;
-            }
-        };
-
-        VolleyController.getInstance(context).addToQueue(stringRequest);
+        VolleyController.getInstance(context).addToQueue(imageRequest);
     }
 
 }
